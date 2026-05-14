49447b2960d457781abad5f2d966310fb8d4d316
ingjulianvega <ingjulianvega@gmail.com>
2026-05-14 12:25:42 -0500

    Probando pipeline automático con ECS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi everyone.
+          Hi esto debe salir automático.
         </p>
         <a
           className="App-link"
